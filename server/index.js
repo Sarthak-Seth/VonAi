9caad3ce2172698395a73772d27f7b2e88e47a47
Sarthak-Seth <sarthakseth211200@gmail.com>
2023-07-26 00:48:29 +0530

    vonAi #3

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
-import postRoutes from './routes/postRoutes.js'
+import postRoutes from "./routes/postRoutes.js";
 import vonAiRoutes from "./routes/vonAiRoutes.js";
-
 
 dotenv.config();
 
